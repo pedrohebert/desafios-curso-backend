e261fe74fb1db1ff7d34c712dfb77c06b65b6cf2
pedrohebert <pedrohebert06@gmail.com>
2026-05-12 12:06:02 -0300

    feat:modelo de ponto de entrega

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from grandes-projetos!")
+from typing import TypedDict
 
+class PontoEntrega(TypedDict):
+    id:int
+    x:float
+    y:float
+
+ponto_de_entrega:list[PontoEntrega] = []
+
+
+def main():
+    pass
 
 if __name__ == "__main__":
     main()
