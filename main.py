e4de0e134de8dfae51182641613cb29ab2f03b8c
pedrohebert <pedrohebert06@gmail.com>
2026-05-12 12:06:54 -0300

    feat:função de distancia entre pontos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import TypedDict
+from math import sqrt
 
 class PontoEntrega(TypedDict):
     id:int
@@ -7,6 +8,8 @@ class PontoEntrega(TypedDict):
 
 ponto_de_entrega:list[PontoEntrega] = []
 
+def distancia(p1:PontoEntrega, p2:PontoEntrega) -> float:
+    return sqrt((p1["x"] - p2["x"])**2 + (p1["y"] - p2["y"])**2 )
 
 def main():
     pass
