c4a66360ee28640761eb00ff5c556a4ee759afe5
pedrohebert <pedrohebert06@gmail.com>
2026-05-12 15:03:28 -0300

    feat: função de distancia entre pontos

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ ponto_de_entrega:list[PontoEntrega] = []
 def distancia(p1:PontoEntrega, p2:PontoEntrega) -> float:
     return sqrt((p1["x"] - p2["x"])**2 + (p1["y"] - p2["y"])**2 )
 
+def create_ponto_entrega(id:int, x:float, y:float,/) -> PontoEntrega:
+    return PontoEntrega(id=id, x=x,y=y, visited=[])
+
 def main():
-    pass
 
 if __name__ == "__main__":
     main()
