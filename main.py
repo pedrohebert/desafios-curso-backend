2a09484e6e55550f67efa85edb6f2eb539a0c17e
pedrohebert <pedrohebert06@gmail.com>
2026-05-15 17:56:15 -0300

    feat: saida da ordem junto com a distancia total

--- main.py
+++ main.py
@@ -50,17 +50,20 @@ def main():
     ponto_de_entrega[4]=(create_ponto_entrega(4, 10, 10))
     ponto_de_entrega[5] = (create_ponto_entrega(5, 2, 3))
 
+    ponto_nulo = create_ponto_entrega(0, 0, 0)
     ordem = []
-    proximo = mais_proximo(create_ponto_entrega(0, 0, 0), ponto_de_entrega)
+    distancia_total = 0
+    proximo = mais_proximo(ponto_nulo, ponto_de_entrega)
 
     while proximo:
         ordem.append(proximo)
+        distancia_total += distancia(ponto_de_entrega[proximo], ponto_de_entrega[ordem[-1]] or ponto_nulo)
         proximo = mais_proximo(
             ponto_de_entrega[ordem[-1]],
             ponto_de_entrega,
         )
 
-    print(ordem)
+    print(dict(ordem = ordem, distancia_total = distancia_total))
 
 
 if __name__ == "__main__":
