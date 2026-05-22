e84342aa65c7f5a58450242cd1d713efb7b1a40d
pedrohebert <pedrohebert06@gmail.com>
2026-05-22 17:17:44 -0300

    refactor(main): retirando codigo desnecessário da main

--- app/main.py
+++ app/main.py
@@ -1,46 +1,8 @@
-from math import sqrt
-from typing import TypedDict
-
-
-class PontoEntrega(TypedDict):
-    id: int
-    x: float
-    y: float
-    visited: list[int]
+from app.PontoEntrega import PontoEntrega
+from app.tools import distancia, create_ponto_entrega, mais_proximo
 
 
 ponto_de_entrega: dict[int, PontoEntrega] = {}
-
-
-def distancia(p1: PontoEntrega, p2: PontoEntrega) -> float:
-    return sqrt((p1["x"] - p2["x"]) ** 2 + (p1["y"] - p2["y"]) ** 2)
-
-
-def create_ponto_entrega(id: int, x: float, y: float, /) -> PontoEntrega:
-    return PontoEntrega(id=id, x=x, y=y, visited=[])
-
-
-def mais_proximo(p: PontoEntrega, ori_pontos: dict[int,PontoEntrega]) -> int:
-
-    proximo = 0
-    menor_dis = 99999999999999999
-
-    pontos = [
-        ponto
-        for id,ponto in ori_pontos.items()
-        if id != p["id"] and id not in p["visited"]
-    ]
-
-    # print(pontos)
-
-    for ponto in pontos:
-        dis = distancia(p, ponto)
-        ponto["visited"].append(p["id"])
-        if dis < menor_dis:
-            proximo = ponto["id"]
-            menor_dis = dis
-
-    return proximo
 
 
 def main():
